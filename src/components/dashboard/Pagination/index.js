e89dc5be74604c3efdf0b99fdc169a7686fadbde
abhishekzrx <abhishekk1717@gmail.com>
2023-05-05 19:25:05 +0530

    2ndpush

--- src/components/dashboard/Pagination/index.js
+++ src/components/dashboard/Pagination/index.js
@@ -4,28 +4,29 @@ import "./styles.css"
 export default function PaginationComponent({page,handlePageChange}) {
   return (
     <div spacing={2}>
-      {/* < Pagination 
+      < Pagination 
         className='pagination'
         count={10} 
         page={page}
         onChange={(event,value)=>handlePageChange(event,value)}
-         sx={{
-                  color:"var(--white)",
-                  "& .Mui-selected":{
-                           backgroundColor:"var(--blue) !important",
-                           color:"#fff !important",
-                           borderColor:"var(--blue) !important",
-                  },
-                  "& .MuiPaginationItem-ellipsis":{
-                           border:"0px solid var(--grey) !important",
-                  },
-                  "&> .MuiPaginationItem-text":{
-                           color:"var(--white)",
-                           border:"1px soild var(--grey)",
-                  },
-         }}
-      /> */}
-      <Pagination
+        sx={{
+          color: "#fff",
+          "& .Mui-selected , .Mui-selected:hover": {
+            backgroundColor: "var(--blue) !important",
+            color: "#fff !important",
+            borderColor: "var(--blue) !important",
+          },
+
+          "& .MuiPaginationItem-ellipsis": {
+            border: "0px solid var(--grey) !important",
+          },
+          "& .MuiPaginationItem-text": {
+            color: "var(--white)",
+            border: "1px solid #333",
+          },
+        }}
+      />
+      {/* <Pagination
         count={10}
         page={page}
         onChange={handlePageChange}
@@ -45,7 +46,7 @@ export default function PaginationComponent({page,handlePageChange}) {
             border: "1px solid #333",
           },
         }}
-      />
+      /> */}
     </div>
   );
 }
